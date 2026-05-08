509abc8614344bbbf7e75a841eff1a33117aad49
11430212 <shane132333806@gmail.com>
2026-05-08 12:00:01 +0800

    A

---
+++ Mc/data.js
@@ -0,0 +1,215 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-2",
+      "name": "麥當勞2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.1999531557557512,
+        "pitch": 0.135163118072537,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.15809376444255818,
+          "pitch": 0.3178379561603659,
+          "rotation": 6.283185307179586,
+          "target": "1-3"
+        },
+        {
+          "yaw": -3.1168474989972506,
+          "pitch": 0.2375924921889876,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.150281515497709,
+          "pitch": -0.11106314192723588,
+          "title": "麥當勞-台北舊宗餐廳",
+          "text": "110臺北市信義區松光里忠孝東路五段522號1 樓<div>更多https://www.youtube.com/shorts/Ay8lynMZ4mE</div><div><img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaCqjVI-N9VI4df1u7TgG8v0ZvuHbZ22aQ-Q&amp;s\" alt=\"麥胞粉絲團| Taipei\"></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-3",
+      "name": "麥當勞3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.015504841615215,
+        "pitch": 0.011310423764175681,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7516736797983334,
+          "pitch": 0.2974110458875501,
+          "rotation": 6.283185307179586,
+          "target": "0-2"
+        },
+        {
+          "yaw": 1.8281292986356013,
+          "pitch": 0.3596836833662671,
+          "rotation": 0,
+          "target": "2-5"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.5232447888720753,
+          "pitch": -0.04154549950581732,
+          "title": "麥當勞-台北南京五餐廳",
+          "text": "104臺北市中山區力行里南京東路三段210號1號<div><img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaCqjVI-N9VI4df1u7TgG8v0ZvuHbZ22aQ-Q&amp;s\" alt=\"麥胞粉絲團| Taipei\"></div>"
+        }
+      ]
+    },
+    {
+      "id": "2-5",
+      "name": "麥當勞5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0025953170610276,
+        "pitch": 0.010012082820194834,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1049555310678123,
+          "pitch": 0.2820915827672561,
+          "rotation": 0,
+          "target": "1-3"
+        },
+        {
+          "yaw": -0.022520017243598645,
+          "pitch": 0.22754347723811463,
+          "rotation": 0,
+          "target": "3-6"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8049307639050642,
+          "pitch": -0.007510832097512576,
+          "title": "麥當勞 台北光復餐廳",
+          "text": "106臺北市大安區正聲里光復南路286號<div><img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaCqjVI-N9VI4df1u7TgG8v0ZvuHbZ22aQ-Q&amp;s\" alt=\"麥胞粉絲團| Taipei\"></div>"
+        }
+      ]
+    },
+    {
+      "id": "3-6",
+      "name": "麥當勞6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.0240452243844267,
+        "pitch": -0.1516279050600975,
+        "fov": 1.325599857056214
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.05830882060667797,
+          "pitch": 0.3551153303587178,
+          "rotation": 0,
+          "target": "2-5"
+        },
+        {
+          "yaw": 2.869053789542491,
+          "pitch": 0.33394376680219473,
+          "rotation": 0,
+          "target": "0-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1424310811348626,
+          "pitch": -0.09836763749590638,
+          "title": "麥當勞",
+          "text": "100臺北市中正區幸市里濟南路二段66號1樓<div><img src=\"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSaCqjVI-N9VI4df1u7TgG8v0ZvuHbZ22aQ-Q&amp;s\" alt=\"麥胞粉絲團| Taipei\"></div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
